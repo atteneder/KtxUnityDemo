a5059a06c77cc55042467742013a7e37638443f7
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-06-22 22:46:41 +0200

    Initial commit 2: Project without binary assets.

---
+++ Assets/Scripts/CustomBasisFileLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BasisUniversalUnity;
+
+public class CustomBasisFileLoader : BasisFileLoader
+{
+    protected override void ApplyTexture(Texture2D texture) {
+        var renderer = GetComponent<Renderer>();
+        if(renderer!=null && renderer.sharedMaterial!=null) {
+            renderer.material.mainTexture = texture;
+        }
+    }
+}
