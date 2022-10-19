ca9145243d5fb9e3f89f1eb9d69f17acbd5263fc
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-10-19 13:10:12 +0200

    refactor: Split logic and UI/component for reusability in tests

--- Assets/Scripts/Benchmark.cs
+++ Assets/Scripts/Benchmark.cs
@@ -12,121 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Profiling;
 using UnityEngine.Networking;
-using Unity.Collections;
 using KtxUnity;
 
-public class Benchmark : MonoBehaviour
+public class Benchmark : IDisposable
 {
-    enum ImageType {
+    internal enum ImageType {
         None,
-        KTX,
+        Ktx,
         PNG,
         JPG
     }
 
-    [SerializeField]
-    private string[] filePaths = null;
+    public delegate void TextureResultDelegate(TextureResult result);
+    public event TextureResultDelegate OnTextureLoaded;
 
-    [SerializeField]
-    Renderer prefab = null;
+    byte[] m_DataArray;
+    ManagedNativeArray m_Data;
+    ImageType m_CurrentType = ImageType.None;
 
-    [SerializeField] float width = 300;
-    [SerializeField] float height = 100;
-    [SerializeField] float yGap = 10;
+    float m_StartTime;
+    float m_BatchTime = -1;
 
-    NativeArray<byte> data;
-    byte[] dataArray;
-    ImageType currentType = ImageType.None;
-    int total_count = 0;
+    CancellationTokenSource m_CancellationTokenSource;
 
-    float start_time;
-    float batch_time = -1;
-    int batch_count = -1;
-
-    float spread = 3;
-    float step = -.001f;
-    float distance = 10;
-    float aspectRatio = 1.5f;
-
-    // Render maximum of 50 at a time (the rest is still in memory)
-    const int MAX_ITEMS = 50;
-    Queue<Renderer> rendererQueue = new Queue<Renderer>(MAX_ITEMS);
-
-    CancellationTokenSource cancellationTokenSource;
-
-    // Start is called before the first frame update
-    void Start() {
-        aspectRatio = Screen.width/(float)Screen.height;
-    }
-
-    void OnGUI() {
-        float x = Screen.width-width;
-        float y = yGap;
-        if(data.IsCreated) {
-            if( GUI.Button( new Rect(x,y,width,height),"Change image")) {
-                data.Dispose();
-                currentType = ImageType.None;
-            }
-            y += height + yGap;
-
-            if( GUI.Button( new Rect(x,y,width,height),"+1")) {
-                LoadBatch(1);
-            }
-            y += height + yGap;
-
-            if( GUI.Button( new Rect(x,y,width,height),"+10")) {
-                LoadBatch(10);
-            }
-            y += height + yGap;
-
-            if( GUI.Button( new Rect(x,y,width,height),"+500")) {
-                LoadBatch(500);
-            }
-            y += height + yGap;
-
-            if( GUI.Button( new Rect(x,y,width,height),"+endless")) {
-                NeverEndingStory();
-            }
-            y += height + yGap;
-        } else
-        if (currentType == ImageType.None) {
-            foreach(var filePath in filePaths) {
-                if( GUI.Button( new Rect(x,y,width,height),filePath)) {
-                    StartCoroutine(LoadData(filePath));
-                }
-                y += height + yGap;
-            }
-        }
-
-        GUI.skin.label.fontSize = 100;
-        GUI.Label(new Rect(0,Screen.height-height-yGap,width,height),total_count.ToString());
-
-        if (batch_time>=0) {
-            GUI.Label(new Rect(Screen.width-width,Screen.height-height-yGap,width,height),batch_time.ToString("0.000"));
-        }
-    }
-
-    IEnumerator LoadData(string filePath) {
+    public IEnumerator LoadData(string filePath) {
         if(filePath.EndsWith(".ktx2")) {
-            currentType = ImageType.KTX;
+            m_CurrentType = ImageType.Ktx;
         } else if(filePath.EndsWith(".png")) {
-            currentType = ImageType.PNG;
+            m_CurrentType = ImageType.PNG;
         } else if(filePath.EndsWith(".jpg")) {
-            currentType = ImageType.JPG;
+            m_CurrentType = ImageType.JPG;
         } else {
             Debug.LogError("Unknown image type");
             yield break;
         }
-        if(data.IsCreated) {
-            data.Dispose();
-        }
+        m_Data?.Dispose();
+        m_DataArray = null;
         var url = TextureBase.GetStreamingAssetsUrl(filePath);
         var webRequest = UnityWebRequest.Get(url);
         yield return webRequest.SendWebRequest();
@@ -134,16 +62,16 @@ public class Benchmark : MonoBehaviour
             Debug.LogErrorFormat("Error loading {0}: {1}",url,webRequest.error);
             yield break;
         }
-        data = new NativeArray<byte>(webRequest.downloadHandler.data,Allocator.Persistent);
-        dataArray = data.ToArray();
+
+        m_DataArray = webRequest.downloadHandler.data;
+        m_Data = new ManagedNativeArray(m_DataArray);
     }
 
-    async void LoadBatch(int count) {
+    public async Task LoadBatch(int count) {
         Profiler.BeginSample("LoadBatch");
-        start_time = Time.realtimeSinceStartup;
-        batch_count = count;
-        batch_time = -1;
-        if (currentType == ImageType.KTX) {
+        m_StartTime = Time.realtimeSinceStartup;
+        m_BatchTime = -1;
+        if (m_CurrentType == ImageType.Ktx) {
             var tasks = new Task[count];
             for (var i = 0; i < count; i++) {
                 var bt = new KtxTexture();
@@ -155,85 +83,53 @@ public class Benchmark : MonoBehaviour
         else {
             for (var i = 0; i < count; i++) {
                 var texture = new Texture2D(2, 2);
-                texture.LoadImage(data.ToArray(), true);
-                ApplyTexture(new TextureResult(texture, TextureOrientation.UNITY_DEFAULT));
+                texture.LoadImage(m_Data.nativeArray.ToArray(), true);
+                OnTextureLoaded?.Invoke(new TextureResult(texture, TextureOrientation.UNITY_DEFAULT));
             }
             Profiler.EndSample();
         }
         
-        batch_time = Time.realtimeSinceStartup-start_time;
-        Debug.LogFormat("Batch load time: {0}", batch_time);
-        batch_count = -1;
+        m_BatchTime = Time.realtimeSinceStartup-m_StartTime;
+        Debug.LogFormat("Batch load time: {0}", m_BatchTime);
     }
 
     async Task LoadAndApply(TextureBase ktx) {
-        var result = await ktx.LoadFromBytes(data);
-        ApplyTexture(result);
+        var result = await ktx.LoadFromBytes(m_Data.nativeArray);
+        OnTextureLoaded?.Invoke(result);
     }
 
-    void NeverEndingStory() {
-        cancellationTokenSource = new CancellationTokenSource();
+    public void NeverEndingStory() {
+        m_CancellationTokenSource = new CancellationTokenSource();
         NeverEndingStoryLoop();
     }
 
     async void NeverEndingStoryLoop() {
-        start_time = Time.realtimeSinceStartup;
-        if(currentType==ImageType.KTX) {
-            while(!cancellationTokenSource.IsCancellationRequested)
+        m_StartTime = Time.realtimeSinceStartup;
+        if(m_CurrentType==ImageType.Ktx) {
+            while(!m_CancellationTokenSource.IsCancellationRequested)
             {
                 var bt = new KtxTexture();
-                var result = await bt.LoadFromBytes(data);
-                if (cancellationTokenSource.IsCancellationRequested) break;
-                ApplyTexture(result);
-                batch_time = Time.realtimeSinceStartup-start_time;
+                var result = await bt.LoadFromBytes(m_Data.nativeArray);
+                if (m_CancellationTokenSource.IsCancellationRequested) break;
+                OnTextureLoaded?.Invoke(result);
+                m_BatchTime = Time.realtimeSinceStartup-m_StartTime;
                 await Task.Yield();
             }
         } else {
-            while(!cancellationTokenSource.IsCancellationRequested)
+            while(!m_CancellationTokenSource.IsCancellationRequested)
             {
                 var texture = new Texture2D(2,2);
-                texture.LoadImage(data.ToArray(),true);
-                if (cancellationTokenSource.IsCancellationRequested) break;
-                ApplyTexture(new TextureResult(texture,TextureOrientation.UNITY_DEFAULT));
-                batch_time = Time.realtimeSinceStartup-start_time;
+                texture.LoadImage(m_DataArray,true);
+                if (m_CancellationTokenSource.IsCancellationRequested) break;
+                OnTextureLoaded?.Invoke(new TextureResult(texture,TextureOrientation.UNITY_DEFAULT));
+                m_BatchTime = Time.realtimeSinceStartup-m_StartTime;
                 await Task.Yield();
             }
         }
     }
 
-    void ApplyTexture(TextureResult result) {
-        Profiler.BeginSample("ApplyTexture");
-        if (result==null) return;
-        total_count++;
-        // Debug.LogFormat("Added image {0}",total_count);
-        var b = Object.Instantiate<Renderer>(prefab);
-        b.transform.position = new Vector3(
-            (Random.value-.5f)* spread * aspectRatio,
-            (Random.value-.5f)* spread,
-            distance
-            );
-        distance+=step;
-        var material = b.material;
-        material.mainTexture = result.texture;
-        var scale = material.mainTextureScale;
-        scale.x = result.orientation.IsXFlipped() ? -1 : 1;
-        scale.y = result.orientation.IsYFlipped() ? -1 : 1;
-        b.material.mainTextureScale = scale;
-
-        rendererQueue.Enqueue(b);
-        while(rendererQueue.Count>MAX_ITEMS) {
-            var r = rendererQueue.Dequeue();
-            if (r == null) return;
-            r.enabled = false;
-        }
-
-        Profiler.EndSample();
-    }
-
-    void OnDestroy() {
-        cancellationTokenSource?.Cancel();
-        if(data.IsCreated) {
-            data.Dispose();
-        }
+    public void Dispose () {
+        m_CancellationTokenSource?.Cancel();
+        m_Data?.Dispose();
     }
 }
---
+++ Assets/Scripts/BenchmarkUI.cs
@@ -0,0 +1,144 @@
+﻿// Copyright (c) 2019-2022 Andreas Atteneder, All Rights Reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+using KtxUnity;
+using ImageType = Benchmark.ImageType;
+
+public class BenchmarkUI : MonoBehaviour
+{
+    [SerializeField]
+    string[] filePaths;
+
+    [SerializeField]
+    Renderer prefab;
+
+    [SerializeField] float width = 300;
+    [SerializeField] float height = 100;
+    [SerializeField] float yGap = 10;
+
+    Benchmark m_Benchmark;
+    ImageType m_CurrentType = ImageType.None;
+    int m_TotalCount;
+
+    float m_StartTime;
+
+    float m_Spread = 3;
+    float m_Step = -.001f;
+    float m_Distance = 10;
+    float m_AspectRatio = 1.5f;
+
+    // Render maximum of 50 at a time (the rest is still in memory)
+    const int k_MaxItems = 50;
+    Queue<Renderer> m_RendererQueue = new Queue<Renderer>(k_MaxItems);
+
+    // Start is called before the first frame update
+    void Start() {
+        m_AspectRatio = Screen.width/(float)Screen.height;
+    }
+
+    void OnGUI() {
+        var x = Screen.width-width;
+        var y = yGap;
+        if(m_Benchmark!=null) {
+            if( GUI.Button( new Rect(x,y,width,height),"Change image")) {
+                m_Benchmark.OnTextureLoaded -= ApplyTexture;
+                m_Benchmark.Dispose();
+                m_Benchmark = null;
+                m_CurrentType = ImageType.None;
+            }
+            y += height + yGap;
+
+            if( GUI.Button( new Rect(x,y,width,height),"+1")) {
+                LoadBatch(1);
+            }
+            y += height + yGap;
+
+            if( GUI.Button( new Rect(x,y,width,height),"+10")) {
+                LoadBatch(10);
+            }
+            y += height + yGap;
+
+            if( GUI.Button( new Rect(x,y,width,height),"+500")) {
+                LoadBatch(500);
+            }
+            y += height + yGap;
+
+            if( GUI.Button( new Rect(x,y,width,height),"+endless")) {
+                NeverEndingStory();
+            }
+        } else
+        if (m_CurrentType == ImageType.None) {
+            foreach(var filePath in filePaths) {
+                if( GUI.Button( new Rect(x,y,width,height),filePath)) {
+                    StartCoroutine(LoadData(filePath));
+                }
+                y += height + yGap;
+            }
+        }
+
+        GUI.skin.label.fontSize = 100;
+        GUI.Label(new Rect(0,Screen.height-height-yGap,width,height),m_TotalCount.ToString());
+    }
+
+    IEnumerator LoadData(string filePath) {
+        m_Benchmark = new Benchmark();
+        yield return m_Benchmark.LoadData(filePath);
+        m_Benchmark.OnTextureLoaded += ApplyTexture;
+    }
+
+    async void LoadBatch(int count) {
+        await m_Benchmark.LoadBatch(count);
+    }
+
+    void NeverEndingStory() {
+        m_Benchmark.NeverEndingStory();
+    }
+
+    void ApplyTexture(TextureResult result) {
+        Profiler.BeginSample("ApplyTexture");
+        if (result==null) return;
+        m_TotalCount++;
+        // Debug.LogFormat("Added image {0}",total_count);
+        var b = Instantiate(prefab);
+        b.transform.position = new Vector3(
+            (Random.value-.5f)* m_Spread * m_AspectRatio,
+            (Random.value-.5f)* m_Spread,
+            m_Distance
+            );
+        m_Distance+=m_Step;
+        var material = b.material;
+        material.mainTexture = result.texture;
+        var scale = material.mainTextureScale;
+        scale.x = result.orientation.IsXFlipped() ? -1 : 1;
+        scale.y = result.orientation.IsYFlipped() ? -1 : 1;
+        b.material.mainTextureScale = scale;
+
+        m_RendererQueue.Enqueue(b);
+        while(m_RendererQueue.Count>k_MaxItems) {
+            var r = m_RendererQueue.Dequeue();
+            if (r == null) return;
+            r.enabled = false;
+        }
+
+        Profiler.EndSample();
+    }
+
+    void OnDestroy() {
+        m_Benchmark?.Dispose();
+    }
+}
